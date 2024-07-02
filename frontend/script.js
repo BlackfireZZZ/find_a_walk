05c86d3271b9f356d6f2d8dbd2f193957991d61c
DrPepper <122201748+DrPepper18@users.noreply.github.com>
2024-07-02 14:51:43 +0300

    Add files via upload

---
+++ frontend/script.js
@@ -0,0 +1,18 @@
+class Profile {
+    constructor(nickname, birthdate, contacts){
+        this.nickname = nickname;
+        this.birthdate = birthdate;
+        this.contacts = contacts;
+    }
+}
+class Event {
+    constructor(){
+
+    }
+    join(){
+
+    }
+    delete(){
+        
+    }
+}
