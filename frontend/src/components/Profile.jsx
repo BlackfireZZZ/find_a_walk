5dfa4e7232a9c8a4abb05ed801b14d54dde8a93a
DrPepper <122201748+DrPepper18@users.noreply.github.com>
2024-07-04 16:27:40 +0300

    Update Profile.jsx

--- frontend/src/components/Profile.jsx
+++ frontend/src/components/Profile.jsx
@@ -1,12 +1,16 @@
 import React from 'react';
 
+function ContactAdd(){
+    
+}
+
 const Profile = ({ user }) => (
     <div id="Profile" style={{display: 'inline-block'}}>
         <div id="username"><h3>{user.nickname}</h3></div>
         <p style={{margin: '-15px 0 10px 0'}}>Nickname</p>
         <div id="userage"><h3>{user.getAge()} y.o.</h3></div>
         <p style={{margin: '-15px 0 10px 0'}}>Age</p>
-        <div id="usercontacts">
+        <div id="usercontacts" style={{padding: "10px"}}>
             {user.contacts.map((contact, index) => (
                 <div
                     key={index}
@@ -15,6 +19,16 @@ const Profile = ({ user }) => (
                     <h5 style={{ color: 'white'}}>{contact}</h5>
                 </div>
             ))}
+            <input 
+            id = "contact_input"
+            type="placeholder" 
+            placeholder="Phone, Telegram, E-mail e.t.c..."
+            style = {{width: "98%"}}></input>
+            <input 
+            type="button" 
+            value="+ Add new contact"
+            style = {{width: "98%"}}
+            onClick={ContactAdd()}></input>
         </div>
     </div>
 );
