ea4447942327026530e89061bf48e8f656641586
DrPepper <122201748+DrPepper18@users.noreply.github.com>
2024-07-05 14:35:37 +0300

    Update Event.jsx

--- frontend/src/components/Event.jsx
+++ frontend/src/components/Event.jsx
@@ -15,7 +15,27 @@ class Event {
         this.count += 1;
     }
 }
-
+function NewEventPanelShow(){
+    //let div = document.getElementById('CreateEvent');
+    //div.style.display = 'block';
+    //console.log(div.style.display);
+}
+function NewEventPanelHide(){
+    //let div = document.getElementById('CreateEvent');
+    //div.style.display = "none";
+}
+function NewEventAdd(){
+    NewEventPanelHide();
+    let name = document.getElementsById('name_input').value;
+    let host = 0;//loggeduser.nickname;
+    let address = 0;
+    let agemin = document.getElementById('agemin_input').value;
+    let agemax = document.getElementById('agemax_input').value;
+    let maxcount = document.getElementById('maxcount_input').value;
+    let date = document.getElementById('date_input').value;
+    console.log(name, host, address, agemin, agemax, maxcount, date);
+    //events.append(new Event(name, host, address, agemin, agemax, maxcount, date));
+}
 const EventComponent = ({ event }) => (
     <div className="Event">
         <div style={{display: 'inline-block'}}>
@@ -26,9 +46,37 @@ const EventComponent = ({ event }) => (
             </h5>
         </div>
         <div style={{display: 'inline-block'}}>
-            <input type="button" value="Let's go!" style={{backgroundColor: 'green', color: 'white'}}></input>
+            <input type="button" value="Я приду" class="ToGoButton"></input>
         </div>
     </div>
 );
+const NewEvent = () => (
+    <div id='CreateEvent'>
+        <div>
+            <h1 style={{display: 'inline-block'}}>Создание нового события</h1>
+            <input type="button" value="X" className='NegativeButton' onClick={NewEventPanelHide()}></input>
+        </div>
+        <div style={{display: 'inline-block', verticalAlign: 'top'}}>
+            <input id="name_input" type="search" placeholder='Название'></input>
+            <br></br>
+            <input id="date_input" type="search" placeholder='Время сбора'></input>
+            <br></br>
+            <p>Точка сбора</p>
+        </div>
+        <div style={{display: 'inline-block'}}>
+            <p style={{display: "inline-block"}}>Мин. возраст</p>
+            <input id="agemin_input" type="range" min="0" max="100" step="1" value="14"></input>
+            <br></br>
+            <p style={{display: "inline-block"}}>Макс. возраст</p>
+            <input id="agemax_input" type="range" min="0" max="100" step="1" value="18"></input>
+            <br></br>
+            <p style={{display: "inline-block"}}>Макс. кол-во человек</p>
+            <input id="maxcount_input" type="range" min="0" max="100" step="1" value="50"></input>
+            <br></br>
+            <input type="submit" placeholder='Опубликовать' className='ToGoButton'></input>
+        </div>
+        
+    </div>
+)
 
-export { Event, EventComponent };
+export { Event, EventComponent, NewEvent };
