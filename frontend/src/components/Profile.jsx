8111eb1809d8508593bfb71212931a074561ceca
DrPepper <122201748+DrPepper18@users.noreply.github.com>
2024-07-09 14:21:07 +0300

    Update Profile.jsx

--- frontend/src/components/Profile.jsx
+++ frontend/src/components/Profile.jsx
@@ -1,36 +1,63 @@
 import React from 'react';
 
-function ContactAdd(){
-    
+class User {
+    constructor(nickname, birthdate, contacts, coords){
+        this.nickname = nickname;
+        this.birthdate = birthdate;
+        this.contacts = contacts;
+        this.coords = coords;
+    }
+    getAge(){
+        return new Date().getFullYear() - this.birthdate[2];
+    }
 }
+let users = [
+    new User('Chinese developers Team',
+        [1, 7, 2006],
+        ['89456783542', 'nightmarefuel'],
+        [55.7558, 37.6176],
+    ),
+    new User('IvanGutche',
+        [1, 7, 2006],
+        ['@stupidcabage']
+    )
+];
+const loggedUser = users[0];
 
+const ContactShow = ({index, contact}) => (
+    <div key={index} className='ContactElement'>
+        <h5 style={{ color: 'white'}}>{contact}</h5>
+    </div>
+);
+const ContactAdd = () => {
+    let div = document.getElementById('usercontacts');
+    let newcontact = document.createElement('div');
+    newcontact.className = "ContactElement";
+    newcontact.innerHTML = `<h5 style="color: white">${document.getElementById('contact_input').value}</h5>`;
+    div.prepend(newcontact);
+}
 const Profile = ({ user }) => (
     <div id="Profile" style={{display: 'inline-block'}}>
         <div id="username"><h3>{user.nickname}</h3></div>
-        <p style={{margin: '-15px 0 10px 0'}}>Nickname</p>
-        <div id="userage"><h3>{user.getAge()} y.o.</h3></div>
-        <p style={{margin: '-15px 0 10px 0'}}>Age</p>
+        <p style={{margin: '-15px 0 10px 0'}}>Псевдоним</p>
+        <div id="userage"><h3>{user.getAge()} лет</h3></div>
+        <p style={{margin: '-15px 0 10px 0'}}>Возраст</p>
         <div id="usercontacts" style={{padding: "10px"}}>
             {user.contacts.map((contact, index) => (
-                <div
-                    key={index}
-                    style={{ borderRadius: '20px', backgroundColor: 'grey', padding: '1px'}}
-                >
-                    <h5 style={{ color: 'white'}}>{contact}</h5>
-                </div>
+                <ContactShow index={index} contact={contact} />
             ))}
             <input 
             id = "contact_input"
             type="placeholder" 
             placeholder="Phone, Telegram, E-mail e.t.c..."
-            style = {{width: "98%"}}></input>
+            style = {{width: "95%"}}></input>
             <input 
             type="button" 
-            value="+ Add new contact"
+            value="+ Добавить контакт"
             style = {{width: "98%"}}
-            onClick={ContactAdd()}></input>
+            onClick={ContactAdd}></input>
         </div>
     </div>
 );
 
-export default Profile;
+export {Profile, users, loggedUser};
