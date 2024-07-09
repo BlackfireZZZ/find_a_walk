bd08e56c9075771a0cfa32172b9dd8d18eeb5c94
DrPepper <122201748+DrPepper18@users.noreply.github.com>
2024-07-09 14:20:14 +0300

    Update Event.jsx

--- frontend/src/components/Event.jsx
+++ frontend/src/components/Event.jsx
@@ -1,49 +1,85 @@
-import React from 'react';
+import React, { useRef } from 'react';
+import {users, loggedUser} from './Profile.jsx'
 
-function NewEventPanelShow(){
-    //let div = document.getElementById('CreateEvent');
-    //div.style.display = 'block';
-    //console.log(div.style.display);
-}
-function NewEventPanelHide(){
-    //let div = document.getElementById('CreateEvent');
-    //div.style.display = "none";
+class Event {
+    constructor(name, host, address, coords, agemin, agemax, date, maxcount) {
+        this.name = name;
+        this.host = host;
+        this.coords = coords;
+        this.radius = 1000;
+        this.address = address;
+        this.agemin = agemin;
+        this.agemax = agemax;
+        this.date = date;
+        this.count = 0;
+        this.maxcount = maxcount;
+        this.memberslist = [];
+    }
+
+    join() {
+        //alert('Тут что-то бэкендерам передать надо')
+    }
 }
 
-function NewEventAdd(){
-    NewEventPanelHide();
-    let name = document.getElementsById('name_input').value;
-    let host = 0;//loggeduser.nickname;
+let events = [
+    new Event(
+        'Чилл без бухла', 
+        users[0].nickname, 
+        'Станция Новокосино', [55, 37],
+        16, 19, '27.07.2024', 5),
+    new Event(
+        'ААА помогите с докером', 
+        users[1].nickname, 
+        'НИУ ВШЭ, Покровский бульвар 11', [56, 38],
+        16, 19, '11.07.2024', 0)
+];
+
+const NewEventPanelShow = () => {
+    let div = document.getElementById('CreateEvent');
+    div.style.display = "block";
+    console.log(div.style.display);
+}
+const NewEventPanelHide = () => {
+    let div = document.getElementById('CreateEvent');
+    div.style.display = "none";
+}
+
+const EventComponent = ({ event }) => (
+    <div className="Event" ref={useRef('CurrentEvents')} style={{padding: '0 20px 0 20px'}}>
+        <div style={{display: 'inline-block'}}>
+            <h2>{event.name}</h2>
+            <p style={{ marginTop: '-10px' }}>{event.host}</p>
+            <h5 style={{ marginTop: '-10px' }}>
+                {event.address}, {event.date}, {event.agemin} - {event.agemax} лет
+            </h5>
+        </div>
+        <div style={{display: 'inline-block', position: 'absolute', right: '0', marginRight: '20px'}}>
+            <input type="button" value="Я приду" class="ToGoButton" onClick={event.join()}></input>
+            {
+                console.log(event.maxcount > 0)
+            }
+            {
+                event.maxcount > 0 ? <h5>{event.count}/{event.maxcount}</h5> : <h5>---</h5>
+            }
+            
+        </div>
+    </div>
+);
+const NewEventAdd = () => {
+    let name = document.getElementById('name_input').value;
+    let host = loggedUser.nickname;
     let address = 0;
+    let coords = [57, 62];
     let agemin = document.getElementById('agemin_input').value;
     let agemax = document.getElementById('agemax_input').value;
     let maxcount = document.getElementById('maxcount_input').value;
     let date = document.getElementById('date_input').value;
-    console.log(name, host, address, agemin, agemax, maxcount, date);
-    //events.append(new Event(name, host, address, agemin, agemax, maxcount, date));
 }
-
-const EventComponent = ({ event }) => (
-    <div className="Event">
-        <h2>{event.name}</h2>
-        <p className="event-host">{event.host.nickname}</p>
-        <h5 className="event-details">
-            {event.address}, {event.date}, {event.agemin} - {event.agemax} лет
-        </h5>
-        <div className="event-tags">
-            {event.tags.map((tag, index) => (
-                <span key={index} className="event-tag">{tag}</span>
-            ))}
-        <div style={{display: 'inline-block'}}>
-            <input type="button" value="Я приду" class="ToGoButton"></input>
-        </div>
-    </div>
-);
 const NewEvent = () => (
     <div id='CreateEvent'>
         <div>
             <h1 style={{display: 'inline-block'}}>Создание нового события</h1>
-            <input type="button" value="X" className='NegativeButton' onClick={NewEventPanelHide()}></input>
+            <input type="button" value="X" className='NegativeButton' onClick={NewEventPanelHide}></input>
         </div>
         <div style={{display: 'inline-block', verticalAlign: 'top'}}>
             <input id="name_input" type="search" placeholder='Название'></input>
@@ -54,18 +90,18 @@ const NewEvent = () => (
         </div>
         <div style={{display: 'inline-block'}}>
             <p style={{display: "inline-block"}}>Мин. возраст</p>
-            <input id="agemin_input" type="range" min="0" max="100" step="1" value="14"></input>
+            <input id="agemin_input" type="placeholder" placeholder="Мин. возраст"></input>
             <br></br>
             <p style={{display: "inline-block"}}>Макс. возраст</p>
-            <input id="agemax_input" type="range" min="0" max="100" step="1" value="18"></input>
+            <input id="agemax_input" type="placeholder" placeholder="Макс. возраст"></input>
             <br></br>
             <p style={{display: "inline-block"}}>Макс. кол-во человек</p>
-            <input id="maxcount_input" type="range" min="0" max="100" step="1" value="50"></input>
+            <input id="maxcount_input" type="placeholder" placeholder="Макс. кол-во участников"></input>
             <br></br>
-            <input type="submit" placeholder='Опубликовать' className='ToGoButton'></input>
+            <input type="submit" placeholder='Опубликовать' className='ToGoButton' onClick={NewEventAdd}></input>
         </div>
         
     </div>
 )
 
-export { Event, EventComponent, NewEvent };
+export { Event, EventComponent, NewEvent, NewEventPanelShow, events };
