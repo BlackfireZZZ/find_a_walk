287b2a9061eda5549cd96ebd2db8365ff2e4699a
Blackfire <Stipundrik@gmail.com>
2024-07-11 11:23:16 +0300

    Added new event request without styles

--- frontend/src/components/NewEvent.jsx
+++ frontend/src/components/NewEvent.jsx
@@ -1,15 +1,15 @@
 import React, { useState, useRef } from 'react';
+import DateTimePicker from 'react-datetime-picker';
 import { users, loggedUser } from './Profile.jsx';
 import { EventObj } from './Event.jsx';
 
-const NewEventAdd = async (nameRef, dateRef, addressRef, ageMinRef, ageMaxRef, maxCountRef, setCords) => {
+const NewEventAdd = async (nameRef, date, addressRef, ageMinRef, ageMaxRef, maxCountRef, setCords) => {
     const name = nameRef.current.value;
     const host = loggedUser.nickname;
     const address = addressRef.current.value;
     const agemin = ageMinRef.current.value;
     const agemax = ageMaxRef.current.value;
     const maxcount = maxCountRef.current.value;
-    const date = dateRef.current.value;
 
     // Fetch coordinates from Yandex Geocoder
     const response = await fetch(`https://geocode-maps.yandex.ru/1.x/?apikey=6997c194-93fd-44c8-89ce-8639d5bcd0c1&geocode=${address}&format=json`);
@@ -26,7 +26,7 @@ const NewEventAdd = async (nameRef, dateRef, addressRef, ageMinRef, ageMaxRef, m
         parseFloat(cords[1]),
         null,
         null,
-        date,
+        date.toISOString(),
         maxcount,
         0,
         []
@@ -42,24 +42,59 @@ const NewEventAdd = async (nameRef, dateRef, addressRef, ageMinRef, ageMaxRef, m
 
 const NewEvent = () => {
     const nameRef = useRef(null);
-    const dateRef = useRef(null);
     const addressRef = useRef(null);
     const ageMinRef = useRef(null);
     const ageMaxRef = useRef(null);
     const maxCountRef = useRef(null);
     const [cords, setCords] = useState([]);
+    const [date, setDate] = useState(new Date());
+    const [suggestions, setSuggestions] = useState([]);
+    const [isDatePickerOpen, setIsDatePickerOpen] = useState(false);
+
+    const handleAddressChange = async (e) => {
+        const address = e.target.value;
+        const response = await fetch(`https://geocode-maps.yandex.ru/1.x/?apikey=6997c194-93fd-44c8-89ce-8639d5bcd0c1&geocode=${address}&format=json`);
+        const data = await response.json();
+        const suggestions = data.response.GeoObjectCollection.featureMember.map(member => member.GeoObject.name);
+        setSuggestions(suggestions);
+    };
+
+    const handleSuggestionClick = (suggestion) => {
+        addressRef.current.value = suggestion;
+        setSuggestions([]);
+    };
+
+    const handleDateChange = (date) => {
+        setDate(date);
+        setIsDatePickerOpen(false); // Close the date picker after selecting a date
+    };
 
     return (
-        <div id="CreateEvent">
+        <div id="CreateEvent" style={{ padding: '20px', border: '1px solid #000' }}>
             <div>
                 <h1 style={{ display: 'inline-block' }}>Создание нового события</h1>
             </div>
             <div style={{ display: 'inline-block', verticalAlign: 'top' }}>
                 <input id="name_input" type="search" placeholder="Название" ref={nameRef} />
                 <br />
-                <input id="date_input" type="search" placeholder="Время сбора" ref={dateRef} />
+                <div>
+                    <DateTimePicker
+                        onChange={handleDateChange}
+                        value={date}
+                        isOpen={isDatePickerOpen}
+                        onCalendarClose={() => setIsDatePickerOpen(false)}
+                        onCalendarOpen={() => setIsDatePickerOpen(true)}
+                    />
+                </div>
                 <br />
-                <input id="address_input" type="search" placeholder="Точка сбора" ref={addressRef} />
+                <input id="address_input" type="search" placeholder="Точка сбора" ref={addressRef} onChange={handleAddressChange} />
+                {suggestions.length > 0 && (
+                    <ul className="suggestions">
+                        {suggestions.map((suggestion, index) => (
+                            <li key={index} onClick={() => handleSuggestionClick(suggestion)}>{suggestion}</li>
+                        ))}
+                    </ul>
+                )}
             </div>
             <div style={{ display: 'inline-block' }}>
                 <p style={{ display: "inline-block" }}>Мин. возраст</p>
@@ -71,7 +106,7 @@ const NewEvent = () => {
                 <p style={{ display: "inline-block" }}>Макс. кол-во человек</p>
                 <input id="maxcount_input" type="text" placeholder="Макс. кол-во участников" ref={maxCountRef} />
                 <br />
-                <input type="submit" value='Опубликовать' className='ToGoButton' onClick={() => NewEventAdd(nameRef, dateRef, addressRef, ageMinRef, ageMaxRef, maxCountRef, setCords)} />
+                <input type="submit" value='Опубликовать' className='ToGoButton' onClick={() => NewEventAdd(nameRef, date, addressRef, ageMinRef, ageMaxRef, maxCountRef, setCords)} />
             </div>
         </div>
     );
