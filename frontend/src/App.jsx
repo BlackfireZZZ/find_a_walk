23c15f55e1fec9e05d5764263113c080fbe8daf1
Blackfire <Stipundrik@gmail.com>
2024-07-11 12:46:17 +0300

    Added working routing

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,33 +1,37 @@
 import React from 'react';
 import './App.css';
+import './index.css'
 import {Profile, loggedUser} from './components/Profile.jsx';
 import YandexMap from './components/YandexMap.jsx';
 import EventsList from "./components/EventsList";
 import {Header} from "./components/Header";
-import { BrowserRouter, Route } from 'react-router-dom';
-
 import {RegScreen} from "./components/Register";
-import {NewEvent} from "./components/NewEvent";
 import {LoginScreen} from "./components/Login";
+import {NewEvent} from "./components/NewEvent";
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+
+
 
 function App() {
     return (
-        <BrowserRouter>
-            <Route path="/" element={
-                <div className="App">
-                    <Header/>
-                    <main>
-                        <Profile user={loggedUser}/>
-                        <div style={{display: 'inline-block', width: '78%'}}>
-                        <YandexMap/>
-                        <EventsList/>
-                        </div>
-                    </main>
-                </div>} />
-            <Route path="/register" element={<RegScreen/>} />
-            <Route path="/login" element={<LoginScreen/>} />
-        </BrowserRouter>
-
+        <Router>
+            <Routes>
+                <Route path="/" element={
+                    <div className="App">
+                        <Header/>
+                        <main>
+                            <Profile user={loggedUser}/>
+                            <div style={{display: 'inline-block', width: '78%'}}>
+                                <YandexMap/>
+                                <EventsList/>
+                            </div>
+                        </main>
+                    </div>}/>
+                <Route path="/register" element={<RegScreen/>}/>
+                <Route path="/login" element={<LoginScreen/>}/>
+                <Route path="/newEvent" element={<NewEvent/>}/>
+            </Routes>
+        </Router>
 
     );
 }
