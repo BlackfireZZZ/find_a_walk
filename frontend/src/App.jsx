4b8c52e871da5629091309ec31897f2deddce170
Blackfire <Stipundrik@gmail.com>
2024-07-11 12:01:29 +0300

    Added react-router-DOM

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,22 +4,31 @@ import {Profile, loggedUser} from './components/Profile.jsx';
 import YandexMap from './components/YandexMap.jsx';
 import EventsList from "./components/EventsList";
 import {Header} from "./components/Header";
+import { BrowserRouter, Route } from 'react-router-dom';
+
 import {RegScreen} from "./components/Register";
 import {NewEvent} from "./components/NewEvent";
+import {LoginScreen} from "./components/Login";
 
 function App() {
     return (
-        <div className="App">
-            <Header/>
-            <main>
-                {/*<Profile user={loggedUser}/>*/}
-                {/*<div style={{display: 'inline-block', width: '78%'}}>*/}
-                {/*    <YandexMap/>*/}
-                {/*    <EventsList/>*/}
-                {/*</div>*/}
-                <NewEvent/>
-            </main>
-        </div>
+        <BrowserRouter>
+            <Route path="/" element={
+                <div className="App">
+                    <Header/>
+                    <main>
+                        <Profile user={loggedUser}/>
+                        <div style={{display: 'inline-block', width: '78%'}}>
+                        <YandexMap/>
+                        <EventsList/>
+                        </div>
+                    </main>
+                </div>} />
+            <Route path="/register" element={<RegScreen/>} />
+            <Route path="/login" element={<LoginScreen/>} />
+        </BrowserRouter>
+
+
     );
 }
 
